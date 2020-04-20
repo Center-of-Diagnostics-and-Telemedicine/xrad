5576ffdbb13e09ac8e7e6d4f91a71c04421d1da2
Alexey Elizarov <a.elizarov@npcmr.ru>
2020-04-20 22:32:51 +0300

    Создан общий проект CMake для библиотеки XRAD.

---
+++ CMake/Project.cmake
@@ -0,0 +1,16 @@
+﻿cmake_minimum_required (VERSION 3.8)
+include_guard()
+
+option(Use_XRAD_BuildDir "Use XRAD common build directory" ON)
+
+if(Use_XRAD_BuildDir)
+	set(XRAD_BuildDir "" CACHE PATH "XRAD library build directory.")
+	if("${XRAD_BuildDir}" STREQUAL "")
+		message(FATAL_ERROR "Please, specify XRAD library build directory (XRAD_BuildDir).")
+	endif()
+	set(XRAD_private_BuildDir "${XRAD_BuildDir}")
+else()
+	set(XRAD_private_BuildDir "${CMAKE_CURRENT_BINARY_DIR}")
+endif()
+
+add_subdirectory("${CMAKE_CURRENT_LIST_DIR}/.." "XRAD")
