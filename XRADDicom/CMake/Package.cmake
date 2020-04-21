177642075d3d15c6eaa25e8df6ce543830d98afc
Alexey Elizarov <a.elizarov@npcmr.ru>
2020-04-21 21:58:15 +0300

    Доработка системы сборки CMake с файлом группы проектов.

--- XRADDicom/CMake/Package.cmake
+++ XRADDicom/CMake/Package.cmake
@@ -2,6 +2,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../../XRADBasic/CMake/PackageSetup.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/GetDependencyLibraries.cmake)
 
+xrad_setup_package("XRADDicom")
 xrad_compose_library_name(XRADDicom_Library "XRADDicom")
 set(XRADDicom_Libraries
 	${XRADDicom_Library}
