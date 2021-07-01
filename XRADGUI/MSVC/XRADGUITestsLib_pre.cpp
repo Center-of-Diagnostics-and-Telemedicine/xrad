6671614a630b6a66326d47e3b7f7cd844523cb77
Nicholas Kulberg <kulberg@yandex.ru>
2021-07-01 14:49:38 +0300

    Тесты перенесены в XRADLibrariesBuild, в проект XRADImmediateTest

--- XRADGUI/MSVC/XRADGUITestsLib_pre.cpp
+++
@@ -1,6 +0,0 @@
-﻿/*
-	Copyright (c) 2021, Moscow Center for Diagnostics & Telemedicine
-	All rights reserved.
-	This file is licensed under BSD-3-Clause license. See LICENSE file for details.
-*/
-#include "pre.h"
